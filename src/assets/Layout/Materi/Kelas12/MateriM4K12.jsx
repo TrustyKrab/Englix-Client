4e35a0f87a6b7b8023e8b9986138e419f80e6eb9
TrustyKrab <fathihpra6303@gmail.com>
2024-09-03 03:13:19 +0700

    fix materi

--- src/assets/Layout/Materi/Kelas12/MateriM4K12.jsx
+++ src/assets/Layout/Materi/Kelas12/MateriM4K12.jsx
@@ -4,9 +4,7 @@ import NavbarHome from '../../Navbar/NavbarHome'
 export default function MateriM4K12() {
     return (
         <div className='bg-blue-100 py-12 font-main'>
-            <div className='mx-auto flex justify-center items-center py-14'>
-                <NavbarHome />
-            </div>
+            <NavbarHome />
             <div className='py-5 px-5 mx-60 mt-20 justify-center rounded-3xl bg-pink-500 text-black'>
                 <div className='bg-gray-100 px-5 py-3 rounded-2xl mb-3'>
                     <h1 className='font-semibold text-xl'>Video ini menjelaskan tentang <span className='font-bold text-2xl'>"Basic English in Introduction"</span></h1>
@@ -18,18 +16,20 @@ export default function MateriM4K12() {
                     <p className='font-semibold text-md'>Pada video materi <span className='text-lg'>"Basic English in Introduction"</span> ini membahas mengenai Pengertian dan elemen-elemen dasar pada Bahasa Inggris. Seperti Greetings, Introduction Yourself, Asking for Personal Information, Basic Responses dan Common Phrases. Dan juga membahas tentang struktur pada Basic Sentences.</p>
                 </div>
             </div>
-            <div>
-                <iframe
-                    width="885"
-                    height="500"
-                    src="https://www.youtube.com/embed/PwIM-l9vOJ4?si=vHEa0zTYskRbQ_gM"
-                    title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerpolicy="strict-origin-when-cross-origin"
-                    allowfullscreen
-                    className="rounded-lg flex shadow-2xl drop-shadow-2xl">
-                </iframe>
+            <div className='mx-auto flex justify-center items-center py-14'>
+                <div>
+                    <iframe
+                        width="885"
+                        height="500"
+                        src="https://www.youtube.com/embed/PwIM-l9vOJ4?si=vHEa0zTYskRbQ_gM"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin"
+                        allowfullscreen
+                        className="rounded-2xl flex shadow-2xl drop-shadow-2xl">
+                    </iframe>
+                </div>
             </div>
         </div>
     )
