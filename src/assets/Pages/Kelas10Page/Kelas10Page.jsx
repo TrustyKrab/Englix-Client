7f583527196003e9a4415ad11ac8c563dfd2accc
TrustyKrab <fathihpra6303@gmail.com>
2024-09-10 04:50:13 +0700

    responsives

--- src/assets/Pages/Kelas10Page/Kelas10Page.jsx
+++ src/assets/Pages/Kelas10Page/Kelas10Page.jsx
@@ -5,13 +5,14 @@ import NavbarHome from '../../Layout/Navbar/NavbarHome'
 export default function Kelas10Page() {
 
     return (
-        <div class="container mx-auto p-4 md:p-6 lg:p-8">
+        <>
             <NavbarHome />
-            <div class="bg-blue-100 py-20 w-full font-main">
-                <div class="flex-1 h-screen md:h-[70vh] lg:h-[80vh]">
+            <div className="bg-blue-100 py-20 w-full font-main">
+
+                <div className="flex-1 h-[77vh] ">
                     <SidebarK10 />
                 </div>
             </div>
-        </div>
+        </>
     )
 }
