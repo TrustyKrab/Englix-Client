8cd75a2a73eec3f9d071a46fabeed81f6b8a54bf
TrustyKrab <fathihpra6303@gmail.com>
2024-09-10 04:30:32 +0700

    responsives

--- src/assets/Pages/RegisterLoginPage/FormForgotPass.jsx
+++ src/assets/Pages/RegisterLoginPage/FormForgotPass.jsx
@@ -24,38 +24,37 @@ export default function FormForgotPass() {
             });
     }
     return (
-        <div className="flex flex-wrap justify-center bg-indigo-300 min-h-screen font-main">
-            <div className="w-full sm:w-1/2 md:w-2/5 border-2 rounded-2xl px-5 sm:px-6 bg-white my-20 sm:py-96 sm:mx-10 py-5">
-                <div className="mb-5">
-                    <h1 className="text-center text-lg sm:text-xl font-semibold">Masukan E-mail anda.</h1>
-                    <h1 className="text-center text-sm sm:text-base">Dan kami akan mengirimkan Link agar anda bisa masuk kembali ke akun anda.</h1>
+        <div className="flex flex-wrap justify-center bg-indigo-300 h-[100vh] font-main">
+            <div className="w-2/5 border-2 rounded-2xl px-6 bg-white my-40 py-5">
+                <div className=" mb-5">
+                    <h1 className="text-center text-lg font-semibold">Masukan E-mail anda.</h1>
+                    <h1 className='text-center'>Dan kami akan mengirimkan Link agar anda bisa masuk kembali ke akun anda.</h1>
                 </div>
                 <form onSubmit={handleSubmit} className="mb-3">
+
                     <label
                         htmlFor="email"
-                        className="block text-gray-700 text-sm font-bold mb-2">Email</label>
+                        className="block text-gray-700 text-sm font-bold mb-2"> Email</label>
                     <input
                         type="email"
                         name="email"
                         id="email"
                         value={email}
+                        placeholder=""
                         onChange={(e) => setEmail(e.target.value)}
                         className="border-2 mb-3 border-slate-200 rounded w-full py-2 px-3 text-gray-700 focus:outline-slate-500 focus:shadow-outline"
                         required
                     />
-                    <div className="flex justify-center mb-5">
-                        <button type="submit" className="bg-pink-500 text-white text-base rounded-lg py-2 px-3 hover:bg-pink-600 transition duration-200">
-                            KIRIM LINK
-                        </button>
+                    <div className='justify-center flex mb-5'>
+                        <button type="submit" className='bg-pink-500 text-white text-base rounded-lg py-2 px-3'>KIRIM LINK</button>
                     </div>
                 </form>
-                <div className="flex justify-center">
-                    <Link to="/FormLogin">
-                        <h1 className="hover:underline text-sm">Kembali</h1>
+                <div className='flex text-center justify-center'>
+                    <Link to='/FormLogin'>
+                        <h1 className='hover:underline'>kembali</h1>
                     </Link>
                 </div>
             </div>
         </div>
-
     )
 }
