302f8237951653a6c11d82800b09349d7e7b8b89
TrustyKrab <fathihpra6303@gmail.com>
2024-09-10 05:48:09 +0700

    responsive

--- src/assets/Pages/Kelas10Page/Kelas10Page.jsx
+++ src/assets/Pages/Kelas10Page/Kelas10Page.jsx
@@ -7,7 +7,7 @@ export default function Kelas10Page() {
         <>
             <NavbarHome />
             <div className="bg-blue-100 py-20 w-full h-screen font-main flex">
-                <div className="flex-1">
+                <div className="flex-1 mt-5">
                     <SidebarK10 />
                 </div>
             </div>
