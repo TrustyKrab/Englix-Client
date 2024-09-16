abcd26f662a0a4f2e2aab18c43513773e8022f63
TrustyKrab <fathihpra6303@gmail.com>
2024-09-17 06:52:10 +0700

    add responsive

--- src/assets/Pages/Kelas10Page/SidebarK10.jsx
+++ src/assets/Pages/Kelas10Page/SidebarK10.jsx
@@ -123,7 +123,7 @@ export default function SidebarK10() {
             <div
                 className={`fixed inset-0 top-16 md:static md:w-1/4 bg-blue-100 text-white md:bg-transparent md:h-auto transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full md:translate-x-0 z-30'}`}
             >
-                <div className="text-lg relative h-[calc(100vh-4rem)] overflow-y-scroll scrollbar-thin px-10 py-10">
+                <div className="text-lg relative h-[100vh] overflow-y-scroll scrollbar-thin px-10 py-10">
                     {coba.map((data, index) => (
                         <a
                             key={index}
